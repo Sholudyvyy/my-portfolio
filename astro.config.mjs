5a59dc4b64e7c590a7977d8a8dea5c77f84946f4
Sholudyvyy <stashohulia@gmail.com>
2026-02-10 12:46:55 +0200

    Init astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,10 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  redirects: {
+    '/code': 'https://github.com/Sholudyvyy',
+    '/tg': 'https://t.me/stanislavhohulia',
+    '/lnkd': 'https://www.linkedin.com/in/stanislav-hohulia/'
+  }
+});
