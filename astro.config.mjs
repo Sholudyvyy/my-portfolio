2505c0eeaf12ad3ef6504677daeff5a32a17f74f
Sholudyvyy <stashohulia@gmail.com>
2026-02-11 15:00:51 +0200

    chore: Remove redirects configuration from astro.config.mjs.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,4 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
-  redirects: {
-    '/code': 'https://github.com/Sholudyvyy',
-    '/tg': 'https://t.me/stanislavhohulia',
-    '/lnkd': 'https://www.linkedin.com/in/stanislav-hohulia/'
-  }
-});
+export default defineConfig({});
